c95b1ee3efc21d9ea915037ba92b6ea8fb94038a
bad9i <54884347+bad9i@users.noreply.github.com>
2019-09-12 10:26:36 +0300

    Give a personalized hello

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            string name;
+            Console.WriteLine("Enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
             Console.ReadKey();
         }
     }
